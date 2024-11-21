99558a5415ded5249d288909669392b97a93ee37
simo <svm.zhang@gmail.com>
2024-11-20 16:46:53 -0800

    Implemented codebase to visualize embedding at 2D space at selected epochs

--- src/w2v/plot_embeds.py
+++ src/w2v/plot_embeds.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 from typing import Mapping
 
+import altair as alt
 import numpy as np
 import polars as pl
 from sklearn.manifold import TSNE
@@ -14,15 +15,36 @@ def transform_tsne(embedding: np.ndarray):
 def plot_embeds(
     embedding_history: Mapping[int, np.ndarray], word_to_ix: Mapping[str, int]
 ) -> None:
-    transformed = transform_tsne(embedding_history[0])
-    pprint(transformed)
-    df = pl.DataFrame(transformed)
-    df = df.with_columns(
-        pl.Series(
-            name="token",
-            values=[k for k in word_to_ix.keys()],
-            dtype=pl.String,
+    for nth in [0, 24, 49]:
+        transformed = transform_tsne(embedding_history[nth])
+        df = pl.DataFrame(transformed)
+        df = df.with_columns(
+            pl.Series(
+                name="token",
+                values=[k for k in word_to_ix.keys()],
+                dtype=pl.String,
+            )
         )
-    )
 
-    pprint(df)
+        out_plot = f"ngram.embeds.{nth}.png"
+        plot = (
+            alt.Chart(df)
+            .mark_circle(size=40)
+            .encode(x="column_0:Q", y="column_1:Q")
+            .properties(
+                width=600,
+                height=400,
+            )
+        ) + (
+            alt.Chart(df)
+            .mark_text(align="left", baseline="middle", dx=5)
+            .encode(
+                x="column_0:Q",
+                y="column_1:Q",
+                text="token:N",
+            )
+        )
+        plot.save(
+            out_plot,
+            ppi=200,
+        )
